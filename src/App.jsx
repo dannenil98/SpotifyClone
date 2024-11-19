902404cbe710a901231d40fb3af6356117e8e0c5
Daniel Nilsson <dantenilsson.98@gmail.com>
2024-11-19 22:06:35 +0100

    added mui theme

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,15 @@
 import './App.css';
+import {Button, Stack} from '@mui/material'
 
 function App() {
 	return (
 		<div className="App">
 			<h1>Techover Self Made - Spotify</h1>
+			<Stack spacing ={2} direction="row">
+				<Button variant="text">Text</Button>
+				<Button variant ="contained">Contained</Button>
+				<Button variant="outlined">Outlined</Button>
+			</Stack>
 		</div>
 	);
 }
